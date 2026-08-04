361f4b1ea4e19ee576fffcb1e62290cb97cb5c30
Suryadeep Banerjee <banerjee.suryadeep007@gmail.com>
2026-08-04 11:15:09 +0530

    Assignment 3

--- Assignment_3/EmailAddress.java
+++ Assignment_3/EmailAddress.java
@@ -1,15 +1,12 @@
 import java.util.*;
 
-public class prg2 {
+public class EmailAddress {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		System.out.print("Enter email address: ");
 		String a = sc.next();
-
 		System.out.println("Email Address : " + a);
-
 		sc.close();
 	}
 }
--- Assignment_3/Sum.java
+++ Assignment_3/Sum.java
@@ -1,16 +1,13 @@
 import java.util.*;
 
-public class prg1 {
+public class Sum {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		System.out.print("Enter 2 numbers: ");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-
 		System.out.println("Sum = " + (a + b));
-
 		sc.close();
 	}
 }
